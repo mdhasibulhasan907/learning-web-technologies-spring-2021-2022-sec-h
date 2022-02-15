1732c50a10c005bf9cb4ef0af9dae8171747ab51
Hasan9842321 <abeer.rocky@gmail.com>
2022-02-16 01:35:10 +0600

    MID_LAB_TASK_06_PHP_SESSION

---
+++ MID_LAB_TASK_06_PHP_SESSION/Registration.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Registration</title>
+</head>
+
+<body>
+    <table border='1'>
+        <tr>
+
+
+            <td>
+
+
+                <table>
+                    <tr>
+
+
+
+
+                        <td align='left' width='1%'>Company</td>
+                        <td align='right'><a href='ret'>Home</a> |
+                            <a href='ret'>Login</a> |
+                            <a href='ret'>Registration</a>
+                        </td>
+
+                    </tr>
+                </table>
+
+
+
+            </td>
+
+
+
+
+
+
+
+
+
+
+        </tr>
+
+        <tr>
+            <td hight="10000px" , width="20000px">
+                <form action="">
+                    <fieldset>
+                        <legend>Registration</legend>
+
+
+
+                        <table>
+                            <tr>
+                                <td>Name: </td>
+                                <td><input type="text" name="" value=""></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <hr>
+                                </td>
+                            </tr>
+                            <tr>
+
+                                <td>Email: </td>
+                                <td><input type="email" name="" value=""></td>
+                            </tr>
+
+                            <tr>
+                                <td colspan="2">
+                                    <hr>
+                                </td>
+                            </tr>
+                            <tr>
+
+                                <td>User Name: </td>
+                                <td><input type="username" name="" value=""></td>
+                            </tr>
+
+                            <tr>
+                                <td colspan="2">
+                                    <hr>
+                                </td>
+                            </tr>
+                            <tr>
+
+                                <td>Password: </td>
+                                <td><input type="password" name="" value=""></td>
+                            </tr>
+
+                            <tr>
+                                <td colspan="2">
+                                    <hr>
+                                </td>
+                            </tr>
+                            <tr>
+
+                                <td>Confirm Password: </td>
+                                <td><input type="ConfirmPassword" name="" value=""></td>
+                            </tr>
+
+                            <tr>
+                                <td colspan="2">
+                                    <hr>
+                                </td>
+                            </tr>
+                            <tr>
+
+
+                                <td colspan="2">
+                                    <from>
+                                        <fieldset>
+                                            <legend>Gender</legend>
+
+
+                                            <input type="radio" name="male" value="gender">Male
+
+                                            <input type="radio" name="female" value="gender">Female
+                                            <input type="radio" name="others" value="gender">Others
+                                        </fieldset>
+                                    </from>
+                                </td>
+                            </tr>
+
+
+                            <tr>
+                                <td colspan="2">
+                                    <hr>
+                                </td>
+                            </tr>
+
+                            <tr>
+
+                                <td>
+                                    <from>
+                                        <fieldset>
+                                            <legend>date of Birth</legend>
+                                            <table>
+
+                                                <tr>
+                                                    <input type="text" name="dd" value=""> /
+
+                                                    <input type="text" name="mm" value=""> /
+                                                    <input type="text" name="yyyy" value="">
+                                                    (dd/mm/yyyy)
+
+                                                </tr>
+
+
+                                            </table>
+
+
+                                        </fieldset>
+                                    </from>
+
+
+                                </td>
+
+
+                            </tr>
+
+                            <td colspan="3">
+                                <hr>
+                            </td>
+
+
+                            <tr>
+                                <td>
+                                    <input type="submit" name="submit" value="submit">
+                                    <input type="reset" name="reset" value="reset">
+
+
+                                </td>
+                            </tr>
+                        </table>
+
+
+
+                    </fieldset>
+
+                </form>
+        </tr>
+
+
+
+
+
+        <tr>
+            <td align='center' , hight="100px" , width="200px">Copyright @ 2017</td>
+        </tr>
+
+
+    </table>
+</body>
+
+</html>
---
+++ MID_LAB_TASK_06_PHP_SESSION/publicuser.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Public User</title>
+</head>
+<body>
+         
+         <table border='1'>
+             
+             <tr>
+                 <td> public user</td>
+                 <td>
+                     <table border='1'>
+                         
+                         
+                    <tr><td>Public Home</td></tr>
+                     <tr><td>Registration</td></tr>
+                  <tr><td>Login</td></tr>
+                  <tr><td>Forget password</td></tr>
+                
+                         
+                     </table>
+                     
+                 </td>
+              </tr>
+                 
+                
+                 
+                
+              
+             
+         </table>
+</body>
+</html>
